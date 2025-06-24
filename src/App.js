9ac945f4036c58b4c623804b5088ebdfcddf5be5
atttul <67624951+atttul@users.noreply.github.com>
2025-06-24 14:50:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import Home from './screens/Home.js';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from 'react-router-dom';
+import Login from './screens/Login.js';
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import Signup from './screens/Signup.js';
+import { CartProvider } from './components/ContextReducer.js';
+import MyOrders from './screens/MyOrders.js';
+
+
+export default function App() {
+    return (
+        <CartProvider>
+            <Router>
+                <Routes>
+                    <Route exact path='/' element={<Home />} />
+                    <Route exact path='/login' element={<Login />} />
+                    <Route exact path='/signup' element={<Signup />} />
+                    <Route exact path='/myorders' element={<MyOrders />} />
+                </Routes>
+            </Router>
+        </CartProvider>
+    );
+}
