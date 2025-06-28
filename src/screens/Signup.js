2ac92474a9e07ee05945a2ca161c9be3222ff057
atttul <67624951+atttul@users.noreply.github.com>
2025-06-28 09:36:15 +0530

    Update Signup.js

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import Navbar from '../components/Navbar';
 
 export default function Signup() {
     const [credentials, setcredentials] = useState({ name: "", email: "", password: "", geolocation: "" });
@@ -26,27 +27,30 @@ export default function Signup() {
     }
 
     return (
-        <div className='container'>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="name" className="form-label">Name</label>
-                    <input type="text" className="form-control" id="exampleInputName1" name='name' value={credentials.name} onChange={onChange} />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email Address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" name='email' value={credentials.email} onChange={onChange} />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" name='password' value={credentials.password} onChange={onChange} />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="address" className="form-label">Address</label>
-                    <input type="text" className="form-control" id="exampleInputAddress1" name='geolocation' value={credentials.geolocation} onChange={onChange} />
-                </div>
-                <button type="submit" className="m-3 btn btn-success">Submit</button>
-                <Link to='/login' className='m-3 btn btn-danger'>Already a user</Link>
-            </form>
+        <div>
+            <div><Navbar /></div>
+            <div className='container'>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="name" className="form-label">Name</label>
+                        <input type="text" className="form-control" id="exampleInputName1" name='name' value={credentials.name} onChange={onChange} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">Email Address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" name='email' value={credentials.email} onChange={onChange} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" name='password' value={credentials.password} onChange={onChange} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="address" className="form-label">Address</label>
+                        <input type="text" className="form-control" id="exampleInputAddress1" name='geolocation' value={credentials.geolocation} onChange={onChange} />
+                    </div>
+                    <button type="submit" className="m-3 btn btn-success">Submit</button>
+                    <Link to='/login' className='m-3 btn btn-danger'>Already a user</Link>
+                </form>
+            </div>
         </div>
     )
 }
